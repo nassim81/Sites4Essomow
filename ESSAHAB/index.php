6724b6c3612731a7a2bca2d1d31d48db6cb4e671
Nassim <oumer90@yahoo.fr>
2020-10-08 09:53:56 +0100

    Commit Alls

---
+++ ESSAHAB/index.php
@@ -0,0 +1,43 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>WebSite</title>
+    <style>
+        body{
+            margin:auto;
+            width : 80%;
+            background-color:#26DA75;
+        }
+        #container{
+            margin-top:20px;
+        }
+        #nav_bar{background-color:#fff;padding:20px;}
+        #nav_bar ul{margin:0px;padding:0px;}
+        #nav_bar li{list-style:none;display:inline;margin-right:10px}
+        #nav_bar li a{text-decoration:none;color:#000;font-weight:bold;}
+    </style>
+</head>
+<body>
+    <div  id=" = container">
+        <div id="nav_bar">
+            <ul>
+                <li><a href="index.php">Home </a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us </a> </li>
+                <li   
+            </ul>
+
+        </div>
+        <div id="content">
+            <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+            ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo 
+            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor 
+            sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+            justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem 
+            ipsum dolor sit amet.
+            </p>
+        </div>
+
+    </div>
+</body>
+</html>
